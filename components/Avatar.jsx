17173c7a4d5c4d0290f1e269ed730c4364a82e82
Mohammad Rezvani <109855184+ImagineNo1@users.noreply.github.com>
2026-05-23 12:59:34 +0330

    Improve hero avatar blending with background

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 
 const Avatar = ({ src = "/avatar.png", opacity = 1 }) => (
-  <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none relative">
-    <Image
-      src={src}
-      alt="avatar"
-      width={737}
-      height={678}
-      className="translate-z-0 w-auto h-auto max-w-[737px] max-h-[678px] object-contain drop-shadow-[0_20px_40px_rgba(0,0,0,0.55)]"
-      style={{ opacity }}
-    />
+  <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none relative items-end justify-end w-full h-full">
+    <div className="relative w-[690px] h-[678px] [mask-image:radial-gradient(circle_at_52%_52%,rgba(0,0,0,1)_56%,rgba(0,0,0,0.82)_70%,rgba(0,0,0,0)_100%)]">
+      <Image
+        src={src}
+        alt="avatar"
+        fill
+        sizes="(min-width: 1280px) 690px, 0px"
+        className="translate-z-0 object-cover object-[52%_42%] drop-shadow-[0_30px_70px_rgba(0,0,0,0.55)] saturate-[1.05] contrast-[1.06]"
+        style={{ opacity }}
+      />
+    </div>
   </div>
 );
 
